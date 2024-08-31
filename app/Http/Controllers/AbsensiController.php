b5410f1fffc3bd07ceacc02758d78ceeedb489e7
navetacandra <naveta.cand@gmail.com>
2024-08-31 11:47:19 +0700

    feat: add controllers, middleware, and routes

---
+++ app/Http/Controllers/AbsensiController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AbsensiController extends Controller
+{
+    public function index()
+    {}
+
+    public function proses_absensi(Request $request)
+    {}
+
+    public function dashboard()
+    {}
+}
